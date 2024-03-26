16f4ac9d4cc0ff86c8d735bc66db8a39471d3b9c
Tino <tino.riebe@gmx.net>
2024-03-26 13:02:01 +0100

    Update docx_explore.py
    save the text between kapitals as dict added

--- docx_explore.py
+++ docx_explore.py
@@ -19,6 +19,31 @@ def extract_table_of_contents(doc_path):
         print('Fehler: ', e)
         return None
 
+def extract_kapitals_and_chapters(doc):
+    try:
+        document = Document(doc)
+        kapitel_und_splitts = {}
+        act_kap = None
+        act_content = []
+        for paragraph in document.paragraphs:
+            paragraph_text = paragraph.text.strip()
+            if paragraph.style.name.startswith('Heading') and paragraph_text:
+                if act_kap is not None:
+                    kapitel_und_splitts[act_kap] = dict(enumerate(act_content))
+                act_kap = paragraph_text
+                act_content = []
+            else:
+                act_content.append(paragraph.text)
+        if act_kap is not None:
+            kapitel_und_splitts[act_kap] = dict(enumerate(act_content))
+        return kapitel_und_splitts
+
+    except Exception as e:
+        print('Fehler: ', e)
+        return None
+
+
 toc = extract_table_of_contents(doc)
 print(toc)
-
+content = extract_kapitals_and_chapters(doc)
+print(content)
